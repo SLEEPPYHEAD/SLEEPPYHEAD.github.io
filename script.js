16a83b0203bee51821630874bedc96028b7a8cef
limerush <mostakim.morshed@g.bracu.ac.bd>
2023-04-18 01:34:22 +0600

    base structure

---
+++ script.js
@@ -0,0 +1,26 @@
+const carouselItems = document.querySelectorAll('.carousel-item');
+let currentItem = 0;
+
+function showItem(index) {
+  carouselItems[currentItem].classList.remove('active');
+  carouselItems[index].classList.add('active');
+  currentItem = index;
+}
+
+setInterval(() => {
+  let index = currentItem + 1;
+  if (index >= carouselItems.length) {
+    index = 0;
+  }
+  showItem(index);
+}, 5000);
+
+var viewer = OpenSeadragon({
+  id: "viewer",
+  prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
+  tileSources: {
+    type: "image",
+    url: "1.jpg"
+  }
+});
+
