6be385ce9000f5bc82dc2ddb63c28dc346b501d8
limerush <mostakim.morshed@g.bracu.ac.bd>
2023-04-20 00:47:43 +0600

    updated

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-const carouselItems = document.querySelectorAll('.carousel-item');
-let currentItem = 0;
+const button = document.querySelector("data-carousel-button")
+buttons.foreach(button => {
+  button.addEventlistener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-caraousel]")
+      .querySelector("[data-slides]")
 
-function showItem(index) {
-  carouselItems[currentItem].classList.remove('active');
-  carouselItems[index].classList.add('active');
-  currentItem = index;
-}
+    const activeSlide = slide.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-setInterval(() => {
-  let index = currentItem + 1;
-  if (index >= carouselItems.length) {
-    index = 0;
-  }
-  showItem(index);
-}, 5000);
+    slides.children[newIndex].dataset.actve = true
+    delete activeSlide.dataset.active
+  })
+})
+// const slide = document.querySelector('.carousel-slide');
+// const prevButton = document.querySelector('.carousel-button-prev');
+// const nextButton = document.querySelector('.carousel-button-next');
+// let slidePosition = 0;
 
-var viewer = OpenSeadragon({
-  id: "viewer",
-  prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
-  tileSources: {
-    type: "image",
-    url: "1.jpg"
-  }
-});
+// function moveToNextSlide() {
+//   if (slidePosition === -100) {
+//     slidePosition = 10;
+//   } else {
+//     slidePosition -= 10;
+//   }
+//   slide.style.transform = `translateX(${slidePosition}%)`;
+// }
 
+// function moveToPrevSlide() {
+//   if (slidePosition === 0) {
+//     slidePosition = -10;
+//   } else {
+//     slidePosition += 10;
+//   }
+//   slide.style.transform = `translateX(${slidePosition}%)`;
+// }
+
+// function startCarousel() {
+//   setInterval(moveToNextSlide, 5000);
+// }
+
+// startCarousel();
+
+// prevButton.addEventListener('click', moveToPrevSlide);
+// nextButton.addEventListener('click', moveToNextSlide);
